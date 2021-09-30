990ce8d47c4642ca6a82f06bf7ee4b04d50c76ca
DanielMorales1103 <mor21785@uvg.edu.gt>
2021-09-30 15:32:41 -0600

    Update Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -16,7 +16,7 @@ public class Usuario{
         boolean espacio = true;
         if(clave.length()>=8){
             largo = true;
-            for (int i =0; i< clave.length ;i++){
+            for (int i =0; i< clave.length() ;i++){
                 char caracter = clave.charAt(i);
                 if(caracter >=65 && caracter <=90){
                     mayuscula = true;
@@ -39,24 +39,35 @@ public class Usuario{
 
     }
 
-    public boolean verificar_usuario(String[][] base){
+    public boolean verificar_usuario(String[][] base, int largo){
         boolean valido = true;
-        for (int i = 0; i<base.size();i++){
-            if(base[i][0] == usuario){
+        for (int i = 0; i<largo;i++){
+            if(base[i][0].equals(nombre)){
                 valido = false;
             }
         }
         return valido;
     }
 
-    public boolean verificar_acceso(String[][] base){
+    public boolean verificar_acceso(String[][] base, int largo){
         boolean valido = false;
-        for (int i = 0; i<base.length;i++){
-            if(base[i][0] == usuario && base[i][1] == clave){
+        for (int i = 0; i<largo;i++){
+            if(base[i][0].equals(nombre) && base[i][1].equals(clave)){
                 valido = true;
             }
         }
         return valido;
+    }
+
+    public int cantidad(String[][] base){
+        int cont = 0;
+        int cont2=0;
+        for (int i = 0; i<base.length;i++){
+            if(base[i][0] != null){
+                cont++;
+            }
+        }
+        return cont;
     }
 
 }
