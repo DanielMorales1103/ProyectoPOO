aceb7a00da57eb44de76600c6148e64f3cbb1549
josep1002 <91641513+josep1002@users.noreply.github.com>
2021-09-30 22:50:21 -0600

    Add files via upload

--- Principal.java
+++ Principal.java
@@ -94,7 +94,7 @@ public class Principal
 
         while(buclePrincipal)
         {
-                respuesta = pregunta("\nMenu:\n1. Agregar nuevo centro médico.\n2. Donacion.\n3. Mostrar 3 centros medicos con menos suministros.\n4. Cerrar sesion.\nRespuesta: ", 4);
+                respuesta = pregunta("\nMenu:\n1. Agregar nuevo centro médico.\n2. Donacion.\n3. Mostrar datos de los centros medicos. \n5. Cerrar sesion.\nRespuesta: ", 4);
                 switch(respuesta)
                 {
                     case 1:
@@ -408,14 +408,41 @@ public class Principal
                         break;
                     
                     case 3:
-                    //codigo para mostrar los 3 centros medicos en peor estado.
-                    System.out.println("3 centros medicos en peor estado");
-
-                        break;
-                    
-                    case 4:
-                    buclePrincipal = false;
-                    sesion = true;
+                        Principal centros;
+                        centros = new Principal();
+                        centros.setNombre(nombre);
+                        centros.setDinero(dinero);
+                        centros.setMedicina(medicinas);
+                        centros.setDoctores(doctores);
+                        centros.setCamas(camas);
+                        centros.setComida(comida);
+                        centros.setTanquesOxigeno(tanquesOxigeno);
+                        centros.setNumero(numero);
+                        centros.setUbicacion(ubicacion);
+                        centros.setPacientes(pacientes);
+                        System.out.print("Nombre del centro: ");
+                        System.out.print(centros.getNombre());
+                        System.out.print("\n");
+                        System.out.print("Localización del centro: ");
+                        System.out.print(centros.getUbicacion());
+                        System.out.print("\n");
+                        System.out.print("Numero de telefono del centro: ");
+                        System.out.print(centros.getNumero());
+                        System.out.print("\n");
+                        System.out.print("Cantidad de doctores del centro: ");
+                        System.out.print(centros.getDoctores());
+                        System.out.print("\n");
+                        System.out.print("Cantidad de pacientes del centro: ");
+                        System.out.print(centros.getPacientes());
+                        System.out.print("\n");
+                        System.out.print("Cantidad de camas del centro: ");
+                        System.out.print(centros.getCamas());
+                        System.out.print("\n");
+                        System.out.print("Cantidad de comida del centro: ");
+                        System.out.print(centros.getComida());
+                        System.out.print("\n");
+                        System.out.print("Cantidad de tanques del centro: ");
+                        System.out.print(centros.getTanquesOxigeno());
                         break;
 
                     default:
@@ -456,4 +483,75 @@ public class Principal
         return respuesta;
     }
 
-}
+
+                    private String nombre;
+                    public String getNombre(){
+                    return nombre;
+                    }
+                    public void setNombre(String nombre){
+                    this.nombre = nombre;
+                    }
+                    private Double dinero;
+                    public Double getDinero(){
+                    return dinero;
+                    }
+                    public void setDinero(Double dinero){
+                    this.dinero = dinero;
+                    }
+                    private Integer medicinas;
+                    public Integer getMedicina(){
+                    return medicinas;
+                    }
+                    public void setMedicina(Integer medicinas){
+                    this.medicinas = medicinas;
+                    }
+                    private Integer doctores;
+                    public Integer getDoctores(){
+                    return doctores;
+                    }
+                    public void setDoctores(Integer doctores){
+                    this.doctores = doctores;
+                    }
+                    private Integer pacientes;
+                    public Integer getPacientes(){
+                    return pacientes;
+                    }
+                    public void setPacientes(Integer pacientes){
+                    this.pacientes = pacientes;
+                    }
+                    private Integer camas;
+                    public Integer getCamas(){
+                        return camas;
+                    }
+                    public void setCamas(Integer camas){
+                        this.camas = camas;
+                    }
+                    private Integer comida;
+                    public Integer getComida(){
+                        return comida;
+                    }
+                    public void setComida(Integer comida){
+                        this.comida = comida;
+                    }
+                    private Integer tanquesOxigeno;
+                    public Integer getTanquesOxigeno(){
+                        return tanquesOxigeno;
+                    }
+                    public void setTanquesOxigeno(Integer tanquesOxigeno){
+                        this.tanquesOxigeno = tanquesOxigeno;
+                    }
+                    private String ubicacion;
+                    public String getUbicacion(){
+                        return ubicacion;
+                    }
+                    public void setUbicacion(String ubicacion){
+                        this.ubicacion = ubicacion;
+                    }
+                    private String numero;
+                    public String getNumero(){
+                        return numero;
+                    }
+                    public void setNumero(String numero){
+                        this.numero = numero;
+                    }
+                }
