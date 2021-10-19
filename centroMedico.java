47e38b5221eee7bc38df608ca4bc323dea042234
eliasalvarado <ealvaradorax@hotmail.com>
2021-10-19 09:03:15 -0600

    Cambios a la clase centroMedico

---
+++ centroMedico.java
@@ -0,0 +1,124 @@
+public class centroMedico
+{
+    private String nombre;
+    private Double dinero;
+    private String numero;
+    private String ubicacion;
+    private Integer tanquesOxigeno;
+    private Integer comida;
+    private Integer camas;
+    private Integer pacientes;
+    private Integer doctores;
+    private Integer medicinas;
+    
+    
+    public String getNombre()
+    {
+        return nombre;
+    }
+    
+    
+    public void setNombre(String nombre)
+    {
+        this.nombre = nombre;
+    }
+    
+    public Double getDinero()
+    {
+        return dinero;
+    }
+    
+    
+    public void setDinero(Double dinero)
+    {
+        this.dinero = dinero;
+    }
+    
+    
+    public Integer getMedicina()
+    {
+        return medicinas;
+    }
+    
+    
+    public void setMedicina(Integer medicinas)
+    {
+        this.medicinas = medicinas;
+    }
+    
+    
+    public Integer getDoctores()
+    {
+        return doctores;
+    }
+    
+    public void setDoctores(Integer doctores)
+    {
+        this.doctores = doctores;
+    }
+    
+    
+    public Integer getPacientes()
+    {
+        return pacientes;
+    }
+    
+    public void setPacientes(Integer pacientes)
+    {
+        this.pacientes = pacientes;
+    }
+    
+    public Integer getCamas()
+    {
+        return camas;
+    }
+    
+    
+    public void setCamas(Integer camas)
+    {
+        this.camas = camas;
+    }
+    
+    
+    public Integer getComida()
+    {
+        return comida;
+    }
+    
+    public void setComida(Integer comida)
+    {
+        this.comida = comida;
+    }
+    
+    
+    public Integer getTanquesOxigeno()
+    {
+        return tanquesOxigeno;
+    }
+    
+    public void setTanquesOxigeno(Integer tanquesOxigeno)
+    {
+        this.tanquesOxigeno = tanquesOxigeno;
+    }
+    
+    public String getUbicacion()
+    {
+        return ubicacion;
+    }
+    
+    public void setUbicacion(String ubicacion)
+    {
+        this.ubicacion = ubicacion;
+    }
+    
+    
+    public String getNumero()
+    {
+        return numero;
+    }
+    
+    public void setNumero(String numero)
+    {
+        this.numero = numero;
+    }
+}
