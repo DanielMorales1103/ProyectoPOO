6960718b80262dc8f8e528c0c9e9fb2331bc96ed
eliasalvarado <ealvaradorax@hotmail.com>
2021-11-19 03:23:45 -0600

    Update Donacion.java
    Se eliminaron propiedades y se agregaron métodos

--- Donacion.java
+++ Donacion.java
@@ -9,42 +9,56 @@ public class Donacion
 {
     private double dinero = 0;
     private int medicinas = 0;
-    private int ropaH = 0;
-    private int ropaM = 0;
     private int camas = 0;
-    private int sabanas = 0;
     private int comida = 0;
     private int tanquesOxigeno = 0;
+
+    public int getTanques()
+    {
+        return this.tanquesOxigeno;
+    }
+
+    public int getComida()
+    {
+        return this.comida;
+    }
+
+    public int getCama()
+    {
+        return this.camas;
+    }
     
+    public int getMedicinas()
+    {
+        return this.medicinas;
+    }
+
+    public double getDinero()
+    {
+        return this.dinero;
+    }
     public Donacion()
     {
 
     }
-
     
     /** 
      * @param medicinas
-     * @param ropaH
-     * @param ropaM
      * @param camas
-     * @param savanas
      * @param comida
      * @param tanquesOxigeno
      * @return String
      */
-    public String suministros(int medicinas, int ropaH, int ropaM, int camas, int sabanas, int comida, int tanquesOxigeno)
+    public String suministros(int medicinas, int camas, int comida, int tanquesOxigeno)
     {
         String mensaje = "";
         //Codigo para agregar suministros a los hospitales.
         this.medicinas = medicinas;
-        this.ropaH = ropaH;
-        this.ropaM = ropaM;
         this.camas = camas;
-        this.sabanas = sabanas;
         this.comida = comida;
         this.tanquesOxigeno = tanquesOxigeno;
 
-        mensaje = "Se ha donado:\nCamas: " + this.camas + "\nMedicinas: " + this.medicinas + "\nRopa para hombres: " + this.ropaH + "\nRopa para mujeres: " + this.ropaM + "\nSavanas: " + this.sabanas + "\nComida: " + this.comida + "\nTanques de oxigeno: " + this.tanquesOxigeno;
+        mensaje = "Se ha donado:\nCamas: " + this.camas + "\nMedicinas: " + this.medicinas + "\nRopa para hombres: " + "\nComida: " + this.comida + "\nTanques de oxigeno: " + this.tanquesOxigeno;
         return mensaje;
     }
 
